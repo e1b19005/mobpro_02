e06077ce9861f0550e50ebf2090ae6487825ea71
e1b19005 <e1b19005@oit.ac.jp>
2021-10-05 14:50:20 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/anaken/kaizi/mobpro_02/Mobpro02Application.java
@@ -0,0 +1,13 @@
+package oit.is.anaken.kaizi.mobpro_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mobpro02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mobpro02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/anaken/kaizi/mobpro_02/Mobpro02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.anaken.kaizi.mobpro_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mobpro02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
